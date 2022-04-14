1d43c2a3b18ec74e7d4626c652f83a82c920f3f2
Matmento <mathiasbrocks@gmail.com>
2022-04-14 20:29:46 +0200

    trying to use "load();" for audio sync issue

--- javascript.js
+++ javascript.js
@@ -2,7 +2,9 @@
        ANIMATION and SOUND
  *************************************/
 
-//puts fart.mp3, gasp.mp3, moan.mp3, wee.mp3 and yell.mp3 in a object
+//https://stackoverflow.com/questions/12953928/immediate-play-sound-on-button-click-in-html-page
+
+//puts all the sound files in a object
 var audio = {
   fart: new Audio("Audio/fart.mp3"),
   gasp: new Audio("Audio/gasp.mp3"),
@@ -10,12 +12,18 @@ var audio = {
   wee: new Audio("Audio/wee.mp3"),
   yell: new Audio("Audio/yell.mp3"),
 };
+//loads all the sounds and logs it to the console
+audio.fart.load(console.log("fart loaded"));
+audio.gasp.load(console.log("gasp loaded"));
+audio.moan.load(console.log("moan loaded"));
+audio.wee.load(console.log("wee loaded"));
+audio.yell.load(console.log("yell loaded"));
 
 /* SILLY */
 //Adds an event listener to the button calling the function that:
 document.getElementById("silly").addEventListener("click", function silly() {
   //
-  //- gets the mp3 variable from the audio object and plays it
+  //- gets the sound variable from the audio object and plays it
   audio.fart.play();
 
   // - makes a variable for getting the button by ID
@@ -32,11 +40,16 @@ document.getElementById("silly").addEventListener("click", function silly() {
 });
 
 /* ANGRY */
+//using console.time to debug audio sync issues
 document.getElementById("angry").addEventListener("click", function angry() {
+  console.time("sound start time");
+  console.time("animation start time");
   audio.yell.play();
+  console.timeEnd("sound start time");
 
   const angry = document.getElementById("angry");
   angry.classList.add("tremble");
+  console.timeEnd("animation start time");
   setTimeout(function () {
     angry.classList.remove("tremble");
   }, 1250);
