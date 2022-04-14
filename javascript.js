30224fbd7eb1f0eb092e7a55cc3709554d53845b
Matmento <mathiasbrocks@gmail.com>
2022-04-14 20:42:55 +0200

    load(); might have fixed the audio issue

--- javascript.js
+++ javascript.js
@@ -1,8 +1,6 @@
-/************************************** 
-       ANIMATION and SOUND
- *************************************/
-
-//https://stackoverflow.com/questions/12953928/immediate-play-sound-on-button-click-in-html-page
+/***************************************** 
+       GETTING the SOUNDS READY
+ *****************************************/
 
 //puts all the sound files in a object
 var audio = {
@@ -12,12 +10,16 @@ var audio = {
   wee: new Audio("Audio/wee.mp3"),
   yell: new Audio("Audio/yell.mp3"),
 };
-//loads all the sounds and logs it to the console
-audio.fart.load(console.log("fart loaded"));
-audio.gasp.load(console.log("gasp loaded"));
-audio.moan.load(console.log("moan loaded"));
-audio.wee.load(console.log("wee loaded"));
-audio.yell.load(console.log("yell loaded"));
+//loads all the sounds to avoid sync issues
+audio.fart.load();
+audio.gasp.load();
+audio.moan.load();
+audio.wee.load();
+audio.yell.load();
+
+/************************************** 
+       ANIMATION and SOUND
+ *************************************/
 
 /* SILLY */
 //Adds an event listener to the button calling the function that:
@@ -29,10 +31,10 @@ document.getElementById("silly").addEventListener("click", function silly() {
   // - makes a variable for getting the button by ID
   const silly = document.getElementById("silly");
 
-  // - uses the variable to add a class to the button
+  // - uses the variable to add a class to the button for the animation
   silly.classList.add("shake");
 
-  // - sets a timer which executes a function after a set amount of time
+  // - sets a timer which executes a function after a set amount of time when the animation is done
   setTimeout(function () {
     // - removes the class from the button after the timer has finished
     silly.classList.remove("shake");
@@ -40,16 +42,11 @@ document.getElementById("silly").addEventListener("click", function silly() {
 });
 
 /* ANGRY */
-//using console.time to debug audio sync issues
 document.getElementById("angry").addEventListener("click", function angry() {
-  console.time("sound start time");
-  console.time("animation start time");
   audio.yell.play();
-  console.timeEnd("sound start time");
 
   const angry = document.getElementById("angry");
   angry.classList.add("tremble");
-  console.timeEnd("animation start time");
   setTimeout(function () {
     angry.classList.remove("tremble");
   }, 1250);
